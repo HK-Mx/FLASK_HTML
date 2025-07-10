8974ece818c9173da837d32c173b59ebf26d3fd2
HK-Mx <maxi.esco@hotmail.com>
2025-07-10 09:41:46 +0200

    jç

--- app.py
+++ app.py
@@ -160,4 +160,4 @@ def hist():
     return jsonify(response.to_dict(orient="records"))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
